d50c73cb6d24e743e1a1b7f1e809473c72a381b1
Pavel Kesso <pavel.kesso@gmail.com>
2018-09-11 06:53:55 +0300

    init TCP server

--- SyncTCPServer/Server/server_TCP.py
+++ SyncTCPServer/Server/server_TCP.py
@@ -1,10 +1,24 @@
+import socketserver
 from Server.server_interface import ServerInterface
 
 
 class TCPServer(ServerInterface):
 
+    def __init__(self,interface = '', ipv4_addr, port, request_handler):
+        self.interface = interface
+        self.ipv4_addr = ipv4_addr
+        self.port = port
+        self.request_handler = request_handler
+
+        # self.socket = socket.socket()
+        # self.socket.bind(self.interface, port) #'' - all interface
+        # self.socket.connect((self.ipv4_addr, self.port))
+        
     def start_server(self):
-        pass
+        with socketserver.TCPServer((self.ipv4_addr, self.port), self.request_handler) as server:
+            self.server = server
+            self.server.serve_forever()
+        
 
     def stop_server(self):
-        pass
+        self.server.shutdown()
